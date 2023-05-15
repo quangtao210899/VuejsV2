a3d196796f84ece4744ecc90bb27a355e7c509c6
Nguyễn Quang Tạo <quangtaobaongoc@gmail.com>
2023-05-15 13:30:05 +0700

    fix sort data

--- src/views/apps/targets/TargetList.vue
+++ src/views/apps/targets/TargetList.vue
@@ -18,37 +18,40 @@
             <!--begin::Card toolbar-->
             <div class="card-toolbar">
                 <!--begin::Toolbar-->
-                <div v-if="selectedIds.length === 0" class="d-flex justify-content-end"
-                    data-kt-subscription-table-toolbar="base">
-                    <!--begin::Export-->
-                    <button type="button"
-                        class="btn btn-sm fw-bold bg-body btn-color-gray-700 btn-active-color-primary me-2"
-                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
-                        <KTIcon icon-name="filter" icon-class="fs-2" />
-                        Filter
-                    </button>
-                    <Fillter @filterData="handleFilter"></Fillter>
-
-                    <!--end::Export-->
-
-                    <!--begin::Add subscription-->
-                    <button type="button" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#kt_modal_new_target_group" @click.passive="handleClick({}, 'add')">
-                        <KTIcon icon-name="plus" icon-class="fs-2" />
-                        Thêm
-                    </button>
-                    <!--end::Add subscription-->
+                <div v-show="selectedIds.length === 0">
+                    <div class="d-flex justify-content-end" data-kt-subscription-table-toolbar="base">
+                        <!--begin::Export-->
+                        <button type="button"
+                            class="btn btn-sm fw-bold bg-body btn-color-gray-700 btn-active-color-primary me-2"
+                            data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
+                            <KTIcon icon-name="filter" icon-class="fs-2" />
+                            Filter
+                        </button>
+                        <Fillter @filterData="handleFilter"></Fillter>
+    
+                        <!--end::Export-->
+    
+                        <!--begin::Add subscription-->
+                        <button type="button" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#kt_modal_new_target_group" @click.passive="handleClick({}, 'add')">
+                            <KTIcon icon-name="plus" icon-class="fs-2" />
+                            Thêm
+                        </button>
+                        <!--end::Add subscription-->
+                    </div>
                 </div>
                 <!--end::Toolbar-->
 
                 <!--begin::Group actions-->
-                <div v-else class="d-flex justify-content-end align-items-center">
-                    <div class="fw-bold me-5">
-                        <span class="me-2">{{ selectedIds.length }}</span>Selected
+                <div v-show="selectedIds.length !== 0">
+                    <div class="d-flex justify-content-end align-items-center">
+                        <div class="fw-bold me-5">
+                            <span class="me-2">{{ selectedIds.length }}</span>Selected
+                        </div>
+                        <button type="button" data-bs-target="#kt_modal_delete" data-bs-toggle="modal" class="btn btn-danger btn-sm ">
+                            Delete Selected
+                        </button>
                     </div>
-                    <button type="button" data-bs-target="#kt_modal_delete" data-bs-toggle="modal" class="btn btn-danger btn-sm ">
-                        Delete Selected
-                    </button>
                 </div>
                 <!--end::Group actions-->
             </div>
